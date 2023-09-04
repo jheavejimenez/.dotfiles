fc77fb2ae9557f1ff1542e8da6249dd67c174d2b
Jheave <jimenezjheave123@gmail.com>
2023-09-04 23:38:59 +0800

    feat: logging message

--- scripts/install.sh
+++ scripts/install.sh
@@ -33,17 +33,21 @@ fi
 # Check if Homebrew is already installed
 if ! command -v brew &>/dev/null; then
     show_status "Installing Homebrew"
+    log "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     show_status "Homebrew is already installed."
+    log "Homebrew is already installed."
 fi
 
 # Check if the Brewfile exists
 if [ -f "$BREWFILE_PATH" ]; then
     show_status "Installing packages from Brewfile"
+    log "Installing packages from Brewfile"
     brew bundle --file="$BREWFILE_PATH"
 else
     show_status "Brewfile not found at $BREWFILE_PATH. Make sure it exists."
+    log "Brewfile not found at $BREWFILE_PATH. Make sure it exists."
 fi
 
 # Define an array of dot file pairs (source and target)
@@ -60,8 +64,10 @@ for pair in "${dotfiles[@]}"; do
     # Check if the source file exists
     if [ -e "$source" ]; then
         show_status "Creating symbolic link for $source"
+        log "Creating symbolic link for $source"
         ln -s "$source" "$target"
     else
         show_status "Source file $source does not exist."
+        log "Source file $source does not exist."
     fi
 done
